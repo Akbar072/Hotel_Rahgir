543669ed4a16e8250a97fe09aa9958ec9fafa7ce
Akbar072 <akbarali.cse.1@gmail.com>
2026-01-21 00:22:58 +0530

    Improve sitemap with all routes

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,42 +1,4 @@
 const menuData = {
-    "Chinese Soups": [
-        "Cream of Tomato Soup",
-        "Hot 'n' Sour Soup",
-        "Manchow Soup"
-    ],
-    "Starters": [
-        "Paneer Chilli Dry",
-        "Veg. Manchurian Dry",
-        "Paneer Manchurian Dry",
-        "Veg. Lollipop",
-        "Veg. Hara Bhara Kabab"
-    ],
-    "Chinese Main Course": [
-        "Veg. Hakka Noodle",
-        "Chilli Garlic Noodle",
-        "Schezwan Noodle",
-        "Paneer Chilli Gravy",
-        "Veg. Manchurian Gravy",
-        "Paneer Manchurian Gravy"
-    ],
-    "Chinese Rice": [
-        "Veg. Fried Rice",
-        "Schezwan Fried Rice",
-        "Mushroom Fried Rice"
-    ],
-    "Salad, Papad & Raita": [
-        "Green Salad",
-        "Tomato Salad",
-        "Veg. Raita",
-        "Boondi Raita",
-        "Pineapple Raita",
-        "Curd",
-        "Buttermilk",
-        "Masala Buttermilk",
-        "Roasted Papad",
-        "Fried Papad",
-        "Masala Papad"
-    ],
     "Tea Time Snacks": [
         "Sp. Tea",
         "Ness coffee",
@@ -62,24 +24,43 @@ const menuData = {
         "Mango Milkshake",
         "Kesar Pista Milkshake"
     ],
-    "Sp. Parcel": [
-        "Paneer Tikka Masala Full",
-        "Chana Masala Full",
-        "Dal Fry Full",
-        "Sp. Paneer Jumbo Pack"
+        "Salad, Papad & Raita": [
+        "Green Salad",
+        "Tomato Salad",
+        "Veg. Raita",
+        "Boondi Raita",
+        "Pineapple Raita",
+        "Curd",
+        "Buttermilk",
+        "Masala Buttermilk",
+        "Roasted Papad",
+        "Fried Papad",
+        "Masala Papad"
     ],
-    "South Indian": [
-        "Aloo Paratha",
-        "Rahgir Sp. Dosa",
-        "Plain Dosa",
-        "Masala Dosa",
-        "Masala Uttapam",
-        "Onion Uttapam",
-        "Tomato Uttapam",
-        "Idli - Sambhar",
-        "Vada Sambhar",
-        "Dahi - Wada",
-        "Sev Usal"
+    "Chinese Soups": [
+        "Cream of Tomato Soup",
+        "Hot 'n' Sour Soup",
+        "Manchow Soup"
+    ],
+    "Starters": [
+        "Paneer Chilli Dry",
+        "Veg. Manchurian Dry",
+        "Paneer Manchurian Dry",
+        "Veg. Lollipop",
+        "Veg. Hara Bhara Kabab"
+    ],
+    "Chinese Main Course": [
+        "Veg. Hakka Noodle",
+        "Chilli Garlic Noodle",
+        "Schezwan Noodle",
+        "Paneer Chilli Gravy",
+        "Veg. Manchurian Gravy",
+        "Paneer Manchurian Gravy"
+    ],
+    "Chinese Rice": [
+        "Veg. Fried Rice",
+        "Schezwan Fried Rice",
+        "Mushroom Fried Rice"
     ],
     "Main Course Indian Veg": [
         "Veg. Sp. Rahgir",
@@ -102,6 +83,24 @@ const menuData = {
         "Dal Fry Butter",
         "Dal Tadka"
     ],
+    "Our Special Vegetable": [
+        "Veg. Bhagwan",
+        "Veg. Hara Bageecha",
+        "Veg. Chatpata",
+        "Veg. Rangila",
+        "Veg. Toofani",
+        "Veg. Tiranga",
+        "Veg. Khazana"
+    ],
+    "Special Dishes": [
+        "Sp. Punjabi Kadhi Full",
+        "Sp. Masala Khichdi Full"
+    ],
+    "Koftas": [
+        "Kaju Kofta",
+        "Malai Kofta",
+        "Cheese Kofta"
+    ],
     "Main Course Indian Paneer": [
         "Paneer Tikka Masala Full",
         "Paneer Tikka Masala",
@@ -114,25 +113,16 @@ const menuData = {
         "Mutter Paneer",
         "Chole Paneer"
     ],
-    "Special Dishes": [
-        "Sp. Punjabi Kadhi Full",
-        "Sp. Masala Khichdi Full"
-    ],
-    "Koftas": [
-        "Kaju Kofta",
-        "Malai Kofta",
-        "Cheese Kofta"
-    ],
-    "Basmati Rice & Pulao": [
-        "Sp. Rahagir Dry Fruit Pulav",
-        "Kaju Pulav",
-        "Veg. Pulav",
-        "Veg. Biryani",
-        "Hyderabadi Biryani",
-        "Jeera Rice",
-        "Plain Rice",
-        "Masala Fried Rice",
-        "Steam Rice"
+    "Our Special Paneer": [
+        "Paneer Mumtaz",
+        "Paneer Mushroom Masala",
+        "Paneer Peshawari",
+        "Paneer Laziz",
+        "Paneer Chatpata",
+        "Paneer Kaju Masala",
+        "Paneer Toofani",
+        "Paneer Lajakat",
+        "Paneer Angara"
     ],
     "Indian Bread": [
         "Chapati",
@@ -145,25 +135,16 @@ const menuData = {
         "Butter Kulcha",
         "Garlic Naan"
     ],
-    "Our Special Paneer": [
-        "Paneer Mumtaz",
-        "Paneer Mushroom Masala",
-        "Paneer Peshawari",
-        "Paneer Laziz",
-        "Paneer Chatpata",
-        "Paneer Kaju Masala",
-        "Paneer Toofani",
-        "Paneer Lajakat",
-        "Paneer Angara"
-    ],
-    "Our Special Vegetable": [
-        "Veg. Bhagwan",
-        "Veg. Hara Bageecha",
-        "Veg. Chatpata",
-        "Veg. Rangila",
-        "Veg. Toofani",
-        "Veg. Tiranga",
-        "Veg. Khazana"
+    "Basmati Rice & Pulao": [
+        "Sp. Rahagir Dry Fruit Pulav",
+        "Kaju Pulav",
+        "Veg. Pulav",
+        "Veg. Biryani",
+        "Hyderabadi Biryani",
+        "Jeera Rice",
+        "Plain Rice",
+        "Masala Fried Rice",
+        "Steam Rice"
     ],
     "Cone": [
         "Butterscotch",
